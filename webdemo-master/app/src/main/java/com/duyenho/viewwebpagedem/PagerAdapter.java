ff145811cdcf042e8ac03939ce78084d41f5b4a3
= <=>
2018-10-27 22:55:30 +0700

    Fragment

---
+++ webdemo-master/app/src/main/java/com/duyenho/viewwebpagedem/PagerAdapter.java
@@ -0,0 +1,37 @@
+package com.duyenho.viewwebpagedem;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+public class PagerAdapter extends FragmentStatePagerAdapter {
+    int mNumOfTabs;
+
+    public PagerAdapter(FragmentManager fm, int NumOfTabs) {
+        super(fm);
+        this.mNumOfTabs = NumOfTabs;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position) {
+            case 0:
+                TabFragmen1 tab1 = new TabFragmen1();
+                return tab1;
+            case 1:
+                TabFragmen2 tab2 = new TabFragmen2();
+                return tab2;
+            case 2:
+                TabFragment3 tab3 = new TabFragment3();
+                return tab3;
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return mNumOfTabs;
+    }
+}
